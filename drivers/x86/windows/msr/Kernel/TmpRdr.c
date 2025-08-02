0399a739bdc48940dc10899138dd8895103277b8
Ger Hobbelt <ger@hobbelt.com>
2025-08-02 18:40:45 +0200

    added the Windows version codes to help a man out (me, setting up `_WIN32_WINNT` so the compiler stops yapping about several internals)

--- drivers/x86/windows/msr/Kernel/TmpRdr.c
+++ drivers/x86/windows/msr/Kernel/TmpRdr.c
@@ -1,3 +1,20 @@
+#if !defined(_WIN32_WINNT_WIN10)
+#define _WIN32_WINNT_WINXP                  0x0501 // Windows XP
+#define _WIN32_WINNT_WS03                   0x0502 // Windows Server 2003
+#define _WIN32_WINNT_WIN6                   0x0600 // Windows Vista
+#define _WIN32_WINNT_VISTA                  0x0600 // Windows Vista
+#define _WIN32_WINNT_WS08                   0x0600 // Windows Server 2008
+#define _WIN32_WINNT_LONGHORN               0x0600 // Windows Vista
+#define _WIN32_WINNT_WIN7                   0x0601 // Windows 7
+#define _WIN32_WINNT_WIN8                   0x0602 // Windows 8
+#define _WIN32_WINNT_WINBLUE                0x0603 // Windows 8.1
+#define _WIN32_WINNT_WINTHRESHOLD           0x0A00 // Windows 10
+#define _WIN32_WINNT_WIN10                  0x0A00 // Windows 10
+#endif
+#ifndef _WIN32_WINNT
+#define _WIN32_WINNT _WIN32_WINNT_WIN10
+#endif
+
 #include <ntddk.h>
 
 #define FILE_DEVICE_UNKNOWN             0x00000022
